fdbc56b3b9ada20407e9cada2889009a89a8af96
unbanksytv <livethelife.tv@gmail.com>
2023-01-15 22:35:44 +0100

    set to goerli

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,37 +1,9 @@
 import type { AppProps } from "next/app";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
-import 'styles/boska-bold.css';
-import 'styles/satoshi.css';
-import 'styles/epilogue.css';
-import {
-  darkTheme,
-  lightTheme,
-  ReservoirKitProvider,
-  ReservoirKitProviderProps,
-  ReservoirKitTheme,
-} from '@reservoir0x/reservoir-kit-ui';
-
-const THEME_SWITCHING_ENABLED = process.env.NEXT_PUBLIC_THEME_SWITCHING_ENABLED
-const DARK_MODE_ENABLED = process.env.NEXT_PUBLIC_DARK_MODE
-const PROXY_API_BASE = process.env.NEXT_PUBLIC_PROXY_API_BASE
-const RESERVOIR_API_KEY = process.env.NEXT_PUBLIC_RESERVOIR_API_KEY
-const BODY_FONT_FAMILY = process.env.NEXT_PUBLIC_BODY_FONT_FAMILY || 'Epilogue'
-const FONT_FAMILY = process.env.NEXT_PUBLIC_FONT_FAMILY || 'Boska'
-const PRIMARY_COLOR = process.env.NEXT_PUBLIC_PRIMARY_COLOR || 'default'
-const DISABLE_POWERED_BY_RESERVOIR =
-  process.env.NEXT_PUBLIC_DISABLE_POWERED_BY_RESERVOIR
-import presetColors from '../colors'
-
-const FEE_BPS = process.env.NEXT_PUBLIC_FEE_BPS
-const FEE_RECIPIENT = process.env.NEXT_PUBLIC_FEE_RECIPIENT
-const SOURCE_DOMAIN = process.env.NEXT_PUBLIC_SOURCE_DOMAIN
-const API_BASE = process.env.NEXT_PUBLIC_RESERVOIR_API_BASE
-const SOURCE_NAME = process.env.NEXT_PUBLIC_SOURCE_NAME
-const CHAIN_ID = process.env.NEXT_PUBLIC_CHAIN_ID
 
 // This is the chainId your dApp will work on.
-const activeChainId = ChainId.Mainnet;
+const activeChainId = ChainId.Goerli;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
